28dd407165f6d6f7f26eb61f0d8bc3e7333693de
bonniepeters <bonnie.e.peters@gmail.com>
2020-02-07 14:45:04 -0700

    deployment

--- dailies_project/settings.py
+++ dailies_project/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'dailies_project.wsgi.application'
 # # Database
 # # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
@@ -128,6 +128,4 @@ STATIC_URL = '/static/'
 
 # Heroku: Update database configuration from $DATABASE_URL. 
 import dj_database_url 
-db_from_env = dj_database_url.config(conn_max_age=500)
-
-DATABASES = ['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)DATABASES['default'].update(db_from_env)

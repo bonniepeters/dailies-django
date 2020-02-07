8a3ee976a1768b46b56dd30ca1c15340b7940065
bonniepeters <bonnie.e.peters@gmail.com>
2020-02-07 14:53:59 -0700

    deployment

--- dailies_project/settings.py
+++ dailies_project/settings.py
@@ -127,5 +127,6 @@ REST_FRAMEWORK = {
 STATIC_URL = '/static/'
 
 # Heroku: Update database configuration from $DATABASE_URL. 
-import dj_database_url 
-db_from_env = dj_database_url.config(conn_max_age=500)DATABASES['default'].update(db_from_env)
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+# db_from_env = dj_database_url.config(conn_max_age=500)DATABASES['default']=db_from_env
